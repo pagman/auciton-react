b3777da4579d7de17857d1504a3d8e0c9b62d6ee
pagman <mantas.pan1@gmail.com>
2022-08-16 17:15:49 +0300

    make card

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,39 @@
-import Layout from "../components/Layout";
+import BasicCard from "../components/Card";
 import SearchAppBar from "../components/navbar";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
 function HomePage() {
   return (
     <div>
       <SearchAppBar />
-      Home Page
-    </div>
+      <div>Home page</div>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+      <BasicCard header='test' title='test' subtitle = 'subtitle' text='a paragraph or sth like that tha is big'/>
+   </div>
   );
 }
 
