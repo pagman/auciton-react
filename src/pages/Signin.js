fb000129b3541bec6ea6d7de86014c25e7f4752b
pagman <mantas.pan1@gmail.com>
2022-08-23 12:52:07 +0300

    forms

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,8 +1,173 @@
+import React, { useState } from "react"
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const defaultValues = {
+  name: "",
+  age: 0,
+  gender: "",
+  os: "",
+  favoriteNumber: 0,
+};
 
 function SigninPage() {
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
   return (
-    <div>
-      <div>Sign in page</div>
+    <div className="center">
+      <div className="floatingDivLeft">
+        <h1 style={{ textAlign: "center" }}>Sign up</h1>
+        <Box
+        onSubmit={handleSubmit}
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              name="name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+              label="Required"
+              defaultValue="Hello World"
+            />
+            <TextField
+              disabled
+              id="outlined-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              id="outlined-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+            <TextField
+              id="outlined-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              id="outlined-search"
+              label="Search field"
+              type="search"
+            />
+            <TextField
+              id="outlined-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+            />
+          </div>
+          <div className="center">
+            <Button variant="outlined" type="submit" >Submit</Button>
+          </div>
+        </Box>
+      </div>
+      <div className="floatingDivLeft">
+        <h1 style={{ textAlign: "center" }}>Log in</h1>
+        <Box
+        onSubmit={handleSubmit}
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              name="age"
+              type="text"
+              value={formValues.age}
+              onChange={handleInputChange}
+              label="Required"
+              defaultValue="Hello World"
+            />
+            <TextField
+              disabled
+              id="outlined-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              id="outlined-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+            <TextField
+              id="outlined-number"
+              label="Number"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              id="outlined-search"
+              label="Search field"
+              type="search"
+            />
+            <TextField
+              id="outlined-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+            />
+          </div>
+
+          <div className="center">
+            <Button variant="outlined" type="submit">Log in</Button>
+          </div>
+        </Box>
+      </div>
     </div>
   );
 }
